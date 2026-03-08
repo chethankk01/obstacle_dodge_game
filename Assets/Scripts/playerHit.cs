fd510c931c7bec9caa9aaa9836a077a57b7251e1
chethankk01 <chethankk01@gmail.com>
2026-03-08 14:37:10 +0530

    Adding start and end game buttons and new scenes

--- Assets/Scripts/playerHit.cs
+++ Assets/Scripts/playerHit.cs
@@ -1,14 +1,28 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerHit : MonoBehaviour
 {
     private int hitCount = 0;
+    private Rigidbody rb;
+    private Move moveScript;
+    [SerializeField] private Canvas gameOverCanvas;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveScript = GetComponent<Move>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
+            gameOverCanvas.enabled = true;
             ChangeColor(collision.gameObject, Color.blue);
+            rb.constraints = RigidbodyConstraints.None;
+            rb.useGravity = true;
+            moveScript.enabled = false;
         }
     }
 
@@ -17,5 +31,10 @@ public class playerHit : MonoBehaviour
         MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
         Material mat = mr.material;
         mat.color = color;
+    }
+
+    public void returnToHome()
+    {
+        SceneManager.LoadSceneAsync("Home");
     }
 }
